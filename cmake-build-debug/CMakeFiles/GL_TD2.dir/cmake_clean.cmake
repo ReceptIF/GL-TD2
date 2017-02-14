089d87dd12ce4daaffb594f7767bd8261def3535
Olivice <olivier.vicente95@gmail.com>
2017-02-14 14:40:50 +0100

    Add CMake system to compile

---
+++ cmake-build-debug/CMakeFiles/GL_TD2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GL_TD2.dir/main.cpp.o"
+  "GL_TD2.pdb"
+  "GL_TD2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GL_TD2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
